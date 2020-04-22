10fd571ded8e5b767e79f731d1a708230b5e028e
schlucht <lothi@schmidschlucht.ch>
2020-04-22 08:10:25 +0200

    render web

--- bankWeb/src/index.ts
+++ bankWeb/src/index.ts
@@ -3,3 +3,6 @@ import { BankReader } from './bankReader';
 
 const bankDates = BankReader.fromCsvText(bankCsvText);
 console.log(bankDates.matches);
+
+const app = document.getElementById('app')! as HTMLDivElement;
+const tbl = document.createElement('table')! as HTMLTableElement;
