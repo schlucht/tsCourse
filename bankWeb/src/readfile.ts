e64254773128c9ba14f090b42a9643e4ff376b3a
schlucht <lothi@schmidschlucht.ch>
2020-04-24 17:01:19 +0200

    new file

--- bankWeb/src/readfile.ts
+++ bankWeb/src/readfile.ts
@@ -1,6 +1,5 @@
 
 import { BankReader } from './bankReader'
 
-const br: BankReader = BankReader.fromCsv('C:/Users/lothi_000/Documents/Programmierung/tsCourse/bank/2019_Test.csv')
+const br: BankReader = BankReader.fromCsv('C:/Users/lothi/Documents/Programmierung/tsCourse/bank/2019_Test.csv')
 
-console.log(br)
